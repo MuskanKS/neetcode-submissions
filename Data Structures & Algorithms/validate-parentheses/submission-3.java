026722e1b679d75409cc87c69b0061493940af72
Muskan Markam <139460392+MuskanKS@users.noreply.github.com>
2026-09-26 23:59:33 +0530

    Add: validate-parentheses - submission-3

---
+++ Data Structures & Algorithms/validate-parentheses/submission-3.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isValid(String s) {
+        // better
+        Stack<Character> stack = new Stack<>();
+
+        for(char ch : s.toCharArray()){
+            // if open bracket than add in the stack
+            if(ch == '(' || ch == '{' || ch == '['){
+                stack.push(ch);
+            }else{
+                // if stack empty then false
+                if(stack.isEmpty()){
+                    return false;
+                }
+                // if closing bracket and same then pop
+                char top = stack.pop();
+                if((top == '(' &&  ch != ')' || top == '[' && ch != ']' || top == '{' && ch != '}')){
+                    return false;
+                }
+            }
+        }
+        // stack will be empty if everything is removed
+        return stack.isEmpty();
+    }
+}
